85596ea0d35f3b85d64901d8fd10950266ae5c7d
Muh Iqbal <muhammadiqbal18081996@gmail.com>
2024-02-29 16:55:41 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { type Request, type Application, type Response } from 'express'
+import 'dotenv/config'
+
+const app: Application = express()
+const port: number =
+  process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`)
+})
